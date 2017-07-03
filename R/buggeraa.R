f0f22c05fd07d92ef27c1a970202cce0ffcc739b
Aidan Morrison <aidandmorrison@gmail.com>
2017-07-03 12:12:32 +1000

    just needed some more a's

--- R/buggeraa.R
+++ R/buggeraa.R
@@ -3,6 +3,6 @@
 buggeraa <- function(df){
   df %>%
     colnames() %>%
-    stringr::str_pad(width = 8, side = "both", pad = "a")
+    stringr::str_pad(width = 14, side = "both", pad = "a")
   return()
 }
