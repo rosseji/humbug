134e12100342603da783f178c5f9a2c3255ab9c9
thebarone <alxmilla@gmail.com>
2017-07-03 12:30:40 +1000

    This i the New Super Size function !!!!!!!!!

---
+++ R/Supersize.R
@@ -0,0 +1,15 @@
+# Super Size Function
+
+#' @name supersize
+#' @title The Supre Size Function!
+#' @param x This is an integer
+#' @return It Supersizes your number!!!!!
+#' @export
+#'
+supersize <- function(x){
+
+  x <- (x^x) * 1000000
+
+  return(x)
+}
+
